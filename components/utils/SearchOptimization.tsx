30873c4db9bf49c88c3a5ad7c7c85e7ed475e006
Calvin Magezi <30309553+CalvinMagezi@users.noreply.github.com>
2022-06-21 09:44:28 +0400

    Update SearchOptimization.tsx

--- components/utils/SearchOptimization.tsx
+++ components/utils/SearchOptimization.tsx
@@ -4,25 +4,28 @@ function SearchOptimization() {
   return (
     <Head>
       <title>Y.M.F</title>
-      <meta name="description" content="Dump Truck Service Provider" />
+      <meta name="description" content="Smart City Project, Hoima Uganda" />
       <meta property="og:type" content="website" />
       <meta
         name="og:title"
         property="og:title"
-        content="Dump Truck Service Provider"
+        content="Smart City Project, Hoima Uganda"
       />
       <meta
         name="og:description"
         property="og:description"
-        content="Dump Truck Service Provider"
+        content="Smart City Project, Hoima Uganda"
       />
-      <meta property="og:site_name" content="A.R.N" />
-      <meta property="og:url" content="https://arn.vercel.app/" />
+      <meta property="og:site_name" content="YMF" />
+      <meta property="og:url" content="https://ymfltd.com" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content="A.R.N" />
-      <meta name="twitter:description" content="Dump Truck Service Provider" />
-      <meta name="twitter:site" content="@arn" />
-      <meta name="twitter:creator" content="@arn" />
+      <meta name="twitter:title" content="YMF" />
+      <meta
+        name="twitter:description"
+        content="Smart City Project, Hoima Uganda"
+      />
+      <meta name="twitter:site" content="@ymfltd" />
+      <meta name="twitter:creator" content="@ymfltd" />
       <link rel="icon" type="image/png" href="/logo.png" />
       <link rel="apple-touch-icon" href="/logo.png" />
       <meta property="og:image" content="/logo.png" />
